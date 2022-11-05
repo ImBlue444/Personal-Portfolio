26948cdbfb3f949859979b5cba9d9daef8f75382
ImBlue <dennis.badagliacca444@gmail.com>
2022-11-05 21:47:42 +0100

    select-none Nav

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-scroll";
 function Navbar() {
   return (
     <div className="sticky top-0 z-20">
-      <ul className="flex bg-black justify-around p-5 border-b-2  ">
+      <ul className="flex select-none bg-black justify-around p-5 border-b-2  ">
         <li className=" cursor-pointer  transition duration-15 text-white hover:text-cyan-400 border-b-2 border-transparent hover:border-cyan-400 ">
           <Link to="home" spy={true} smooth={true} offset={50} duration={500}>
             Home
